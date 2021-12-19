0673d1197758c6df00547bc3ef98d040b84f4a66
Shehan Sanjula <39646768+ShehanSanjula@users.noreply.github.com>
2021-12-19 11:31:41 +0530

    Update consodisa5.js

--- js/consodisa5.js
+++ js/consodisa5.js
@@ -1,14 +1,20 @@
-const disableDevtools = callback => {
-  const original = Object.getPrototypeOf;
+"use strict";
 
-  Object.getPrototypeOf = (...args) => {
-    if (Error().stack.includes("getCompletions")) callback();
-    return original(...args);
-  };
-};
+window.onload = function(){
 
-disableDevtools(() => {
-  console.error("devtools has been disabled");
+  var o = {x:1};
 
-  while (1);
-});
+  //Make "x" non-configurable
+  Object.defineProperty(o, "x", {configurable: false});
+  //freeze "o";
+  Object.freeze(o);
+
+  console.log(Object.getOwnPropertyDescriptor(o, "x"));
+  //outputs => Object { value: 1, writable: true, enumerable: true, configurable: false }
+  console.log(Object.isSealed(o));
+  //outputs => true
+
+  Object.defineProperty(o, "x", {writable: true}); //Now this doesn't cause.
+  console.log(Object.getOwnPropertyDescriptor(o, "x"));
+  //outputs => Object { value: 1, writable: false, enumerable: true, configurable: false }
+}
