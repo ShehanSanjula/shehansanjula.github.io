6d566b5aa1a671890e8e70959a14a245f977f591
Shehan Sanjula <39646768+ShehanSanjula@users.noreply.github.com>
2021-12-19 12:13:11 +0530

    Update consodisa.js

--- js/consodisa.js
+++ js/consodisa.js
@@ -6,27 +6,12 @@ document.addEventListener("keydown", function(event){
     var key = event.key || event.keyCode;
 
     if (key == 123) {
+        console.error("devtools has been disabled"),
         return false;
     } else if ((event.ctrlKey && event.shiftKey && key == 73) || (event.ctrlKey && event.shiftKey && key == 74)) {
+        console.error("devtools has been disabled"),
         return false;
     }
 }, false);
 
 //throw "Warning! You are not authorized to access the developer console of Shehan's website.";
-
-(function() {
-    try {
-        var $_console$$ = console;
-        Object.defineProperty(window, "console", {
-            get: function() {
-                if ($_console$$._commandLineAPI)
-                    throw "Sorry, for security reasons, the script console is deactivated on shehansanjula.me";
-                return $_console$$
-            },
-            set: function($val$$) {
-                $_console$$ = $val$$
-            }
-        })
-    } catch ($ignore$$) {
-    }
-})();
