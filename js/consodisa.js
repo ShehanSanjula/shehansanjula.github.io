f831b143558ac76e212ccc53768cdd161f6acdd8
Shehan Sanjula <39646768+ShehanSanjula@users.noreply.github.com>
2021-12-19 12:17:08 +0530

    Update consodisa.js

--- js/consodisa.js
+++ js/consodisa.js
@@ -6,10 +6,8 @@ document.addEventListener("keydown", function(event){
     var key = event.key || event.keyCode;
 
     if (key == 123) {
-        console.error("devtools has been disabled"),
         return false;
     } else if ((event.ctrlKey && event.shiftKey && key == 73) || (event.ctrlKey && event.shiftKey && key == 74)) {
-        console.error("devtools has been disabled"),
         return false;
     }
 }, false);
