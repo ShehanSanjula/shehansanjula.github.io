7f916e70d80599cedd06649aa61306a1a9fba226
Shehan Sanjula <39646768+ShehanSanjula@users.noreply.github.com>
2021-12-19 12:48:44 +0530

    Update consodisa.js

--- js/consodisa.js
+++ js/consodisa.js
@@ -1,2 +1,2 @@
 window.oncontextmenu=function(){return!1},document.addEventListener("keydown",(function(e){var n=e.key||e.keyCode;return 123!=n&&(!(e.ctrlKey&&e.shiftKey&&73==n||e.ctrlKey&&e.shiftKey&&74==n)&&void 0)}),!1);
-//"Warning! for security reasons, the script console is deactivated on https://shehansanjula.me by Shehan Sanjula."
+//"Warning! for security reasons, some script console features on https://shehansanjula.me has been deactivated by Shehan Sanjula."
