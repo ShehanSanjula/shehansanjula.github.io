91ead66aed45ac1d7ddfc1c3a1d35698464655bb
Shehan Sanjula <39646768+ShehanSanjula@users.noreply.github.com>
2021-12-19 12:36:07 +0530

    Update consodisa.js

--- js/consodisa.js
+++ js/consodisa.js
@@ -1,2 +1,2 @@
 window.oncontextmenu=function(){return!1},document.addEventListener("keydown",(function(e){var n=e.key||e.keyCode;return 123!=n&&(!(e.ctrlKey&&e.shiftKey&&73==n||e.ctrlKey&&e.shiftKey&&74==n)&&void 0)}),!1);
-//"Warning! You are not authorized to input developer console commands for Shehan's website."
+//"Warning! You are not authorized to access developer console commands for Shehan's website."
