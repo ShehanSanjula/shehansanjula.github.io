9638a3f78b883a4e5c3eecfe4a16ec7e729d2fe7
Shehan Sanjula <39646768+ShehanSanjula@users.noreply.github.com>
2021-12-19 12:20:43 +0530

    Update consodisa.js

--- js/consodisa.js
+++ js/consodisa.js
@@ -6,8 +6,10 @@ document.addEventListener("keydown", function(event){
     var key = event.key || event.keyCode;
 
     if (key == 123) {
+        console.warn("testett"),
         return false;
     } else if ((event.ctrlKey && event.shiftKey && key == 73) || (event.ctrlKey && event.shiftKey && key == 74)) {
+        console.warn("testett"),
         return false;
     }
 }, false);
